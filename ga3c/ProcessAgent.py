c5630cffc50af45fc5645ac05eb3048164f98b26
lacibeb <lacibeb@gmail.com>
2018-05-28 11:32:25 +0200

    action in experience

--- ga3c/ProcessAgent.py
+++ ga3c/ProcessAgent.py
@@ -64,7 +64,10 @@ class ProcessAgent(Process):
 
     def convert_data(self, experiences):
         x_ = np.array([exp.state for exp in experiences])
-        a_ = np.eye(self.num_actions)[np.array([exp.action for exp in experiences])].astype(np.float32)
+        #discreat eaction
+        #a_ = np.eye(self.num_actions)[np.array([exp.action for exp in experiences])].astype(np.float32)
+        #continuous action
+        a_ = np.array([exp.action for exp in experiences])
         r_ = np.array([exp.reward for exp in experiences])
         return x_, r_, a_
 
