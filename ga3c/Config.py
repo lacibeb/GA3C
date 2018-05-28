4dbecd37ae5bf355314a317cdb019d068cc9cb0e
lacibeb <lacibeb@gmail.com>
2018-05-28 10:17:20 +0200

    default player name added to start game nem ez volt a hiba mert start game nek player kell nem draw

--- ga3c/Config.py
+++ ga3c/Config.py
@@ -46,11 +46,11 @@ class Config:
     
     # If the dynamic configuration is on, these are the initial values.
     # Number of Agents
-    AGENTS = 32 
+    AGENTS = 1
     # Number of Predictors
-    PREDICTORS = 2
+    PREDICTORS = 1
     # Number of Trainers
-    TRAINERS = 2
+    TRAINERS = 1
 
     # Device
     DEVICE = 'gpu:0'
