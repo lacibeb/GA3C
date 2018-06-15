d1f85937f21cbfcb0058f94cf51b89022f46e8ca
lacibeb <lacibeb@gmail.com>
2018-06-15 11:57:58 +0200

    debug disabled prediction from run_episode In Thread prediction only waits for normal agent so with hr agents it hangs because they are not getting prediction

--- ga3c/ProcessHRAgent.py
+++ ga3c/ProcessHRAgent.py
@@ -33,58 +33,11 @@ import time
 from Config import Config
 from Environment import Environment
 from Experience import Experience
+from ProcessAgent import ProcessAgent
 
-
-class ProcessHRAgent(Process):
+class ProcessHRAgent(ProcessAgent):
     def __init__(self, id, prediction_q, training_q, episode_log_q):
-        super(ProcessHRAgent, self).__init__()
-
-        self.id = id
-        self.prediction_q = prediction_q
-        self.training_q = training_q
-        self.episode_log_q = episode_log_q
-
-        self.env = Environment()
-        self.num_actions = self.env.get_num_actions()
-        self.actions = np.arange(self.num_actions)
-
-        self.discount_factor = Config.DISCOUNT
-        # one frame at a time
-        self.wait_q = Queue(maxsize=1)
-        self.exit_flag = Value('i', 0)
-
-    @staticmethod
-    def _accumulate_rewards(experiences, discount_factor, terminal_reward):
-        reward_sum = terminal_reward
-        for t in reversed(range(0, len(experiences)-1)):
-            r = np.clip(experiences[t].reward, Config.REWARD_MIN, Config.REWARD_MAX)
-            reward_sum = discount_factor * reward_sum + r
-            experiences[t].reward = reward_sum
-        return experiences[:-1]
-
-    def convert_data(self, experiences):
-        x_ = np.array([exp.state for exp in experiences])
-        #discreat eaction
-        #a_ = np.eye(self.num_actions)[np.array([exp.action for exp in experiences])].astype(np.float32)
-        #continuous action
-        a_ = np.array([exp.action for exp in experiences])
-        a_ = np.reshape(a_, newshape=[len(a_), 1])
-        r_ = np.array([exp.reward for exp in experiences])
-        return x_, r_, a_
-
-    def predict(self, state):
-        # put the state in the prediction q
-        self.prediction_q.put((self.id, state))
-        # wait for the prediction to come back
-        p, v = self.wait_q.get()
-        return p, v
-
-    def select_action(self, prediction):
-        if Config.PLAY_MODE:
-            action = np.argmax(prediction)
-        else:
-            action = np.random.choice(self.actions, p=prediction)
-        return action
+        super(ProcessHRAgent, self).__init__(id, prediction_q, training_q, episode_log_q)
 
     def run_episode(self):
         self.env.reset()
@@ -100,11 +53,14 @@ class ProcessHRAgent(Process):
                 self.env.step(0)  # 0 == NOOP
                 continue
 
-            prediction, value = self.predict(self.env.current_state)
+            #prediction, value = self.predict(self.env.current_state)
             # arcade
             # action = self.select_action(prediction)
             # contonuous
-            action = prediction[0]
+
+            # TODO human reference action
+            action = 0
+            
             action = action + np.random.uniform(0.03, -0.03)
 
             reward, done = self.env.step(int(action * 180.0))
@@ -127,17 +83,3 @@ class ProcessHRAgent(Process):
                 reward_sum = 0.0
 
             time_count += 1
-
-    def run(self):
-        # randomly sleep up to 1 second. helps agents boot smoothly.
-        time.sleep(np.random.rand())
-        np.random.seed(np.int32(time.time() % 1 * 1000 + self.id * 10))
-
-        while self.exit_flag.value == 0:
-            total_reward = 0
-            total_length = 0
-            for x_, r_, a_, reward_sum in self.run_episode():
-                total_reward += reward_sum
-                total_length += len(r_) + 1  # +1 for last frame that we drop
-                self.training_q.put((x_, r_, a_))
-            self.episode_log_q.put((datetime.now(), total_reward, total_length))
