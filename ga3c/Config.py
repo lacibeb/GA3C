069eed9f0e1cdcd284c7157e01bc4b2f8b167633
lacibeb <lacibeb@gmail.com>
2018-06-15 11:40:18 +0200

    debug

--- ga3c/Config.py
+++ ga3c/Config.py
@@ -46,7 +46,7 @@ class Config:
     
     # If the dynamic configuration is on, these are the initial values.
     # Number of Agents
-    AGENTS = 63
+    AGENTS = 1
     # Number of human reference Agents
     HUMAN_REF_AGENTS = 1
     # Number of Predictors
