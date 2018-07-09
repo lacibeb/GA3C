17f9ead2ab09b8eefc6a25355516a0d4f79d90cc
lacibeb <lacibeb@gmail.com>
2018-07-09 11:43:46 +0200

    print in Processagent removed

--- ga3c/ProcessAgent.py
+++ ga3c/ProcessAgent.py
@@ -121,7 +121,6 @@ class ProcessAgent(Process):
                 action = prediction[0]
             else:
                 action = self.select_action(prediction)
-                print('action: '+str(action))
                 # converting discrate action to continuous
                 # converting -1 .. 1 to fixed angles
                 action = (2 / Config.CONTINUOUS_INPUT_PARTITIONS) * action - 1
