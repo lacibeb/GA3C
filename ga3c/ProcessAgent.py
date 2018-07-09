21ff35e88b3ecfd4810eba38349db776e06c97aa
lacibeb <lacibeb@gmail.com>
2018-07-09 13:30:04 +0200

    debug

--- ga3c/ProcessAgent.py
+++ ga3c/ProcessAgent.py
@@ -83,7 +83,8 @@ class ProcessAgent(Process):
         else:
             #discreate action
             print("num actions: " + str(self.num_actions))
-            print(*experiences.action, sep='\n')
+            for exp in experiences:
+                print(exp.action, sep='\n')
             a_ = np.eye(self.num_actions)[np.array([exp.action for exp in experiences])].astype(np.float32)
         a_ = np.reshape(a_, newshape=[len(a_), 1])
         r_ = np.array([exp.reward for exp in experiences])
