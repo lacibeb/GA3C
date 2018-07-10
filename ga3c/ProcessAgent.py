56d65f95a834d2eae962622995857fb7283ec8c3
lacibeb <lacibeb@gmail.com>
2018-07-10 16:04:31 +0200

    debug convert

--- ga3c/ProcessAgent.py
+++ ga3c/ProcessAgent.py
@@ -171,14 +171,18 @@ class ProcessAgent(Process):
         # two nearest action probability will be bigger, others will be 0
         # from the two nearest, probabilities are linear
         prediction = [0.0]*Config.CONTINUOUS_INPUT_PARTITIONS
-        for i in range(Config.CONTINUOUS_INPUT_PARTITIONS - 1):
+        for i in range(Config.CONTINUOUS_INPUT_PARTITIONS):
             error = i - (action + 1) / (2 / Config.CONTINUOUS_INPUT_PARTITIONS)
             if abs(error) > 1.0:
                 prediction[i] = 0
             else:
                 if error < 0.0:
-                    prediction[i] = 1.0 + error
-                    prediction[i + 1] = -1.0*error
+                    if i == Config.CONTINUOUS_INPUT_PARTITIONS - 1:
+                        prediction[i] = 1.0 + error
+                        prediction[0] = -1.0 * error
+                    else:
+                        prediction[i] = 1.0 + error
+                        prediction[i + 1] = -1.0*error
         # print(str(action) + " " + str(discrate_action) + " " + str(prediction))
         return prediction
 
