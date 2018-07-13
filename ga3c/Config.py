ec5efbc7c55ba193066bb424f3300577f11d22d9
lacibeb <lacibeb@gmail.com>
2018-07-13 15:30:06 +0200

    RMS_prop gradiens clipping implemented debug

--- ga3c/Config.py
+++ ga3c/Config.py
@@ -46,7 +46,7 @@ class Config:
     
     # If the dynamic configuration is on, these are the initial values.
     # Number of Agents
-    AGENTS = 5c
+    AGENTS = 5
     # Number of human reference Agents from Agents AGENTS=humref+regular
     HUMAN_REF_AGENTS = 2
     # Number of Predictors
