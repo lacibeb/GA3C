acd20fc8e5b3f7460b1d863984256f56f1a533f7
lacibeb <lacibeb@gmail.com>
2018-07-16 12:34:30 +0200

    debug

--- ga3c/NetworkTester.py
+++ ga3c/NetworkTester.py
@@ -55,12 +55,16 @@ class NetworkTester(ProcessAgent):
                         # converting discrate action to continuous
                         # converting -1 .. 1 to fixed angles
                         env_action = self.convert_action_discrate_to_angle(action)
-                    color = np.array([int(round((env_action + 1) * 127)), 0, 0], dtype='uint8')
+                    # color = np.array([int(round((env_action + 1) * 127)), 0, 0], dtype='uint8')
+                    color = int(round((env_action + 1) * 127))
                     x_.append(i)
                     y_.append(j)
                     c_.append(color)
             if use_matplotlib:
-                plt.plot([x for x in x_], [y for y in y_], [c for c in c_])
+                # plt.plot([x for x in x_], [y for y in y_], [c for c in c_])
+                plt.scatter(x_,y_, c=color, alpha=0.3,
+                            cmap='viridis')
+                plt.colorbar();
                 plt.pause(0.001)
                 plt.draw()
                 plt.savefig('try1.png')
