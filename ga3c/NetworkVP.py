485139317474bc0ec677670ec952f64b70f10345
lacibeb <lacibeb@gmail.com>
2018-07-23 14:34:26 +0200

    debug no input . . .

--- ga3c/NetworkVP.py
+++ ga3c/NetworkVP.py
@@ -246,7 +246,7 @@ class Network:
         return prediction
     
     def predict_p_and_v(self, x):
-        print("xout: " + str(x))
+        print("xin: " + str(x))
         print("xout: " + str(self.sess.run(self.x, feed_dict={self.x: x})))
         print("out: " + str(self.sess.run(self.p_d1, feed_dict={self.x: x})))
         return self.sess.run([self.softmax_p, self.logits_v], feed_dict={self.x: x})
