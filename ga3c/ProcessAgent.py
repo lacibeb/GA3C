b4a4c6d1215006774a861aff239b5b454d99dd44
lacibeb <lacibeb@gmail.com>
2018-09-05 14:58:03 +0200

    try without rendering

--- ga3c/ProcessAgent.py
+++ ga3c/ProcessAgent.py
@@ -131,16 +131,16 @@ class ProcessAgent(Process):
                 self.env.step(None)  # 0 == NOOP
                 continue
 
-            # print('state: ' + str(self.env.current_state))
+            print('state: ' + str(self.env.current_state))
             prediction, value = self.predict(self.env.current_state)
-            # print('pred: ' + str(prediction))
+            print('pred: ' + str(prediction))
             if Config.DISCRATE_INPUT:
                 action = self.select_action(self.actions, prediction)
             else:
                 action = prediction
 
             reward, done = self.env.step(action)
-
+            print('reward: ' + str(reward))
             reward_sum += reward
             exp = Experience(self.env.previous_state, action, prediction, reward, self.env.current_state, done)
             experiences.append(exp)
@@ -152,10 +152,6 @@ class ProcessAgent(Process):
                 updated_exps = ProcessAgent._accumulate_rewards(experiences, self.discount_factor, terminal_reward)
                 x_, r_, a_, x2_, done_ = self.convert_data(updated_exps)
 
-                print('action')
-                print(str(a_.transpose()))
-                print('reward')
-                print(str(r_.transpose()))
                 yield x_, r_, a_, x2_, done_, reward_sum
 
                 # reset the tmax count
