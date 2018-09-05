68d07de311ad660ae62d40b5885f1f7438998bb4
lacibeb <lacibeb@gmail.com>
2018-09-05 15:09:40 +0200

    try without rendering

--- ga3c/ProcessAgent.py
+++ ga3c/ProcessAgent.py
@@ -148,10 +148,10 @@ class ProcessAgent(Process):
             if done or self.time_count == Config.TIME_MAX:
                 #terminal_reward = 0 if done else value
                 # with pyperrace the final reward is always in last step, it always plays until the end
-                terminal_reward = reward
+                terminal_reward = reward_sum
                 updated_exps = ProcessAgent._accumulate_rewards(experiences, self.discount_factor, terminal_reward)
                 x_, r_, a_, x2_, done_ = self.convert_data(updated_exps)
-
+                print(r_)
                 yield x_, r_, a_, x2_, done_, reward_sum
 
                 # reset the tmax count
