dabda1ef1501e554a88843c90de7c04b0a594398
lacibeb <lacibeb@gmail.com>
2018-09-05 15:42:13 +0200

    bigger network

--- ga3c/Config.py
+++ ga3c/Config.py
@@ -115,7 +115,7 @@ class Config:
     LEARNING_RATE_END = 0.000003
 
     #Network structure
-    DENSE_LAYERS = (1024, 1024)
+    DENSE_LAYERS = (10240, 1024)
     # RMSProp parameters
     # if False than ADAM optimizer only for ddpg
     RMSPROP = True
