9bf59e28f42de32d8834c87c5f741e6fd0806e32
lacibeb <lacibeb@gmail.com>
2018-09-06 16:10:58 +0200

    tweak

--- ga3c/Config.py
+++ ga3c/Config.py
@@ -115,7 +115,7 @@ class Config:
     BETA_END = 0.5
 
     # Learning rate
-    LEARNING_RATE_START = 0.0003
+    LEARNING_RATE_START = 0.003
     LEARNING_RATE_END = 0.000003
 
     #Network structure
