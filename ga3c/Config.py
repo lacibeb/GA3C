466c7784edf8139a488308833c17f1b631098be5
lacibeb <lacibeb@gmail.com>
2018-09-06 16:53:48 +0200

    tweak

--- ga3c/Config.py
+++ ga3c/Config.py
@@ -119,7 +119,7 @@ class Config:
     LEARNING_RATE_END = 0.00003
 
     #Network structure
-    DENSE_LAYERS = (16,)
+    DENSE_LAYERS = (16,16)
     # RMSProp parameters
     # if False than ADAM optimizer only for ddpg
     RMSPROP = True
