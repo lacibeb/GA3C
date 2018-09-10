56a1b5941c17c1e1342e02d18c515241e327e9f4
lacibeb <lacibeb@gmail.com>
2018-09-10 16:23:51 +0200

    no exploraion

--- ga3c/Config.py
+++ ga3c/Config.py
@@ -54,7 +54,7 @@ class Config:
     # Enable to train
     TRAIN_MODELS = True
     # Load old models. Throws if the model doesn't exist
-    LOAD_CHECKPOINT = True
+    LOAD_CHECKPOINT = False
     # If 0, the latest checkpoint is loaded
     LOAD_EPISODE = 0 
 
