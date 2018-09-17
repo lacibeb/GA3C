2e835d211d3663d8babc4fff6600ce6f35581652
lacibeb <lacibeb@gmail.com>
2018-09-17 15:33:47 +0200

    debug ddpg

--- ga3c/ProcessAgent.py
+++ ga3c/ProcessAgent.py
@@ -117,7 +117,7 @@ class ProcessAgent(Process):
 
     def predict(self, state):
         # put the state in the prediction q
-        print(str(state))
+        # print(str(state))
         self.prediction_q.put((self.id, state))
         # wait for the prediction to come back
         p, v = self.wait_q.get(timeout=2)
@@ -148,7 +148,7 @@ class ProcessAgent(Process):
                 self.env.step(None)  # 0 == NOOP
                 continue
 
-            # print('state: ' + str(self.env.current_state))
+            print('state: ' + str(self.env.current_state))
             prediction, value = self.predict(self.env.current_state)
             print('pred: ' + str(prediction))
 
