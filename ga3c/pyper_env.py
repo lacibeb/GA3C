462b2657347e7b28a63847e6cf7367d6c360222e
lacibeb <lacibeb@gmail.com>
2018-11-08 14:24:42 +0100

    debug paper race dist in dist out edge of map

--- ga3c/pyper_env.py
+++ ga3c/pyper_env.py
@@ -676,7 +676,17 @@ class PaperRaceEnv:
 
                 crosses, distance, section_nr, start, finish = self.sectionpass(self.pos, self.v)
 
-                distance = max(0, min(distance, Config.LIDAR_MAX_LENGTH))
+                for section in self.outside_sections:
+                    cross, distanceout = self.check_if_crossed(self.pos, direction, self.section)
+                    if cross:
+                        break
+
+                for section in self.inside_sections:
+                    cross, distancein = self.check_if_crossed(self.pos, direction, self.section)
+                    if cross:
+                        break
+
+                distance = max(0, min(distancein, distanceout, Config.LIDAR_MAX_LENGTH))
 
                 self.lidar_channels[i] = distance
 
